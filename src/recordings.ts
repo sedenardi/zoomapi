428ab85aa022eac150ca2faa6feea11344a0c0b6
steelbrain <i@steelbrain.me>
2021-04-26 21:55:57 +0500

    :new: Add API for GetAccountRecordings

---
+++ src/recordings.ts
@@ -0,0 +1,51 @@
+import { ZoomMeetingRecordingFileType, ZoomMeetingRecordingType } from './common';
+import request from './util/request';
+
+export type GetAccountRecordingsParams = {
+  page_size?: number;
+  next_page_token?: string;
+  from?: string;
+  to?: string;
+};
+
+export type GetAccountRecordingsResponse = {
+  next_page_token: string;
+  from: string;
+  to: string;
+  page_size: number;
+  total_records: number;
+  meetings: {
+    uuid: string;
+    id: string;
+    host_id: string;
+    topic: string;
+    start_time: string;
+    duration: number;
+    total_size: number;
+    recording_count: number;
+    recording_files: {
+      id: string;
+      recording_start: string;
+      recording_end: string;
+      file_type: ZoomMeetingRecordingFileType;
+      file_size: number;
+      play_url: string;
+      download_url: string;
+      status: string;
+      recording_type: ZoomMeetingRecordingType;
+      meeting_id: string;
+    }[];
+  }[];
+};
+
+export default function recordings(zoomRequest: ReturnType<typeof request>) {
+  return {
+    GetAccountRecordings(accountId: string, params?: GetAccountRecordingsParams) {
+      return zoomRequest<GetAccountRecordingsResponse>({
+        method: 'GET',
+        path: `/accounts/${accountId}/recordings`,
+        params,
+      });
+    }
+  };
+}
