40bccb25f3de5159d49de6f4f69b8555069ef3de
Anees Iqbal <hello@aneesiqbal.me>
2021-09-01 13:58:53 +0400

    :new: Add methods for metrics get meetings API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import users from './users';
 import meetings from './meetings';
 import webinars from './webinars';
+import metrics from './metrics';
 import reports from './reports';
 import { ZoomOptions } from './common';
 import request from './util/request';
@@ -12,6 +13,7 @@ export default function(zoomApiOpts: ZoomOptions) {
     users: users(zoomRequest),
     meetings: meetings(zoomRequest),
     webinars: webinars(zoomRequest),
+    metrics: metrics(zoomRequest),
     reports: reports(zoomRequest),
   };
 }
@@ -20,4 +22,5 @@ export * from './webhooks';
 export * from './common';
 export * from './users';
 export * from './meetings';
+export * from './metrics';
 export * from './reports';
