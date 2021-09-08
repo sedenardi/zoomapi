480a062e3eff7ebdfd31eaf9ce8faced567ddde8
Anees Iqbal <hello@aneesiqbal.me>
2021-09-08 21:17:31 +0300

    :new: Add metrics get meetings API

---
+++ src/metrics.ts
@@ -0,0 +1,30 @@
+import { MetricMeeting, PaginatedResponse } from './common'
+import request from './util/request'
+
+export type GetMetricsMeetingsParams = {
+  type?: 'past' | 'pastOne' | 'live'
+  from: string
+  to: string
+  page_size?: number
+  page_next_token?: string
+  include_fields?: 'tracking_fields'
+}
+
+export type GetMetricsMeetingsResponse = PaginatedResponse & {
+  from: string
+  to: string
+  next_page_token: string
+  meetings: MetricMeeting[]
+}
+
+export default function metrics(zoomRequest: ReturnType<typeof request>) {
+  return {
+    GetMeetings(params?: GetMetricsMeetingsParams) {
+      return zoomRequest<GetMetricsMeetingsResponse>({
+        method: 'GET',
+        path: '/metrics/meetings',
+        params,
+      })
+    },
+  }
+}
